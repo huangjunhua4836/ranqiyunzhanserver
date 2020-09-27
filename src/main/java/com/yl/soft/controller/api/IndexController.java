e0c7df9d2adf86253a3df5ad62925049a3784e1b
huangjunhua <740920822@qq.com>
2020-09-27 14:14:24 +0800

    资讯接口，云端橱窗接口、企业橱窗接口

--- src/main/java/com/yl/soft/controller/api/IndexController.java
+++ src/main/java/com/yl/soft/controller/api/IndexController.java
@@ -1,6 +1,5 @@
 package com.yl.soft.controller.api;
 
-import cn.hutool.core.util.ArrayUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -10,10 +9,11 @@ import com.yl.soft.controller.base.BaseController;
 import com.yl.soft.dict.CommonDict;
 import com.yl.soft.dto.AppLoginDTO;
 import com.yl.soft.po.EhbArticle;
+import com.yl.soft.po.EhbExhibitor;
+import com.yl.soft.po.EhbOpportunity;
 import com.yl.soft.service.EhbArticleService;
-import com.yl.soft.service.EhbAudienceService;
 import com.yl.soft.service.EhbExhibitorService;
-import com.yl.soft.service.EhbLabelService;
+import com.yl.soft.service.EhbOpportunityService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,9 +31,7 @@ import java.util.Map;
 @RequestMapping("/api")
 public class IndexController extends BaseController {
     @Autowired
-    private EhbAudienceService ehbAudienceService;
-    @Autowired
-    private EhbLabelService ehbLabelService;
+    private EhbOpportunityService ehbOpportunityService;
     @Autowired
     private EhbExhibitorService ehbExhibitorService;
     @Autowired
@@ -57,7 +55,7 @@ public class IndexController extends BaseController {
             ,@ApiResponse(code = -1, message = "系统异常")
     })
     @PostMapping("/randExibitionList")
-    public BaseResponse<List<AppLoginDTO>> randExibitionList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
+    public BaseResponse<PageInfo<AppLoginDTO>> randExibitionList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
         if(StringUtils.isEmpty(paramMap.get("pageNum"))){
             return setResultError(403,"","当前页码不能为空！");
         }
@@ -83,6 +81,7 @@ public class IndexController extends BaseController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "token", value = "用户登陆后获取token",paramType = "query",required = true)
             ,@ApiImplicitParam(name = "pageNum", value = "当前页数", required = true, paramType = "query")
+            ,@ApiImplicitParam(name = "title", value = "资讯标题", paramType = "query")
     })
     @ApiResponses({
             @ApiResponse(code = 200, message = "成功")
@@ -92,12 +91,13 @@ public class IndexController extends BaseController {
             ,@ApiResponse(code = -1, message = "系统异常")
     })
     @PostMapping("/articleList")
-    public BaseResponse<List<EhbArticle>> articleList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
+    public BaseResponse<PageInfo<EhbArticle>> articleList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
         if(StringUtils.isEmpty(paramMap.get("pageNum"))){
             return setResultError(403,"","当前页码不能为空！");
         }
         QueryWrapper<EhbArticle> ehbArticleQueryWrapper = new QueryWrapper<>();
         ehbArticleQueryWrapper.eq("isdel",CommonDict.CORRECT_STATE);
+        ehbArticleQueryWrapper.eq(!StringUtils.isEmpty(paramMap.get("title")),"title",paramMap.get("title"));
         ehbArticleQueryWrapper.orderByDesc("releasetime");
 
         Integer pageParam[] = pageValidParam(paramMap);
@@ -107,4 +107,73 @@ public class IndexController extends BaseController {
         return setResultSuccess(new PageInfo<>(ehbArticles));
     }
 
+    /**
+     * 商品橱窗列表
+     * @return
+     */
+    @ApiOperation(value = "商品橱窗列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "token", value = "用户登陆后获取token",paramType = "query",required = true)
+            ,@ApiImplicitParam(name = "pageNum", value = "当前页数", required = true, paramType = "query")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "成功")
+            ,@ApiResponse(code = 401, message = "token为空！")
+            ,@ApiResponse(code = 402, message = "token失效！")
+            ,@ApiResponse(code = 403, message = "参数不合法请检查必填项")
+            ,@ApiResponse(code = -1, message = "系统异常")
+    })
+    @PostMapping("/opportunityList")
+    public BaseResponse<PageInfo<EhbOpportunity>> opportunityList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
+        if(StringUtils.isEmpty(paramMap.get("pageNum"))){
+            return setResultError(403,"","当前页码不能为空！");
+        }
+        AppLoginDTO appLoginDTO = getCurrAppLogin(paramMap.get("token").toString());
+        QueryWrapper<EhbOpportunity> ehbOpportunityQueryWrapper = new QueryWrapper<>();
+        ehbOpportunityQueryWrapper.eq("isdel",CommonDict.CORRECT_STATE);
+        ehbOpportunityQueryWrapper.eq("createuser",appLoginDTO.getId());//必须是本企业
+        ehbOpportunityQueryWrapper.eq("type",2);//类别是商品
+        ehbOpportunityQueryWrapper.orderByDesc("releasetime");
+
+        Integer pageParam[] = pageValidParam(paramMap);
+        PageHelper.startPage(pageParam[0], pageParam[1]);
+        List<EhbOpportunity> ehbOpportunities = ehbOpportunityService.list(ehbOpportunityQueryWrapper);
+        Collections.shuffle(ehbOpportunities);
+        return setResultSuccess(new PageInfo<>(ehbOpportunities));
+    }
+
+    /**
+     * 云端橱窗列表
+     * @return
+     */
+    @ApiOperation(value = "云端橱窗列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "token", value = "用户登陆后获取token",paramType = "query",required = true)
+            ,@ApiImplicitParam(name = "pageNum", value = "当前页数", required = true, paramType = "query")
+            ,@ApiImplicitParam(name = "enterprisename", value = "企业名称",  paramType = "query")
+            ,@ApiImplicitParam(name = "boothno", value = "展位号", paramType = "query")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "成功")
+            ,@ApiResponse(code = 401, message = "token为空！")
+            ,@ApiResponse(code = 402, message = "token失效！")
+            ,@ApiResponse(code = 403, message = "参数不合法请检查必填项")
+            ,@ApiResponse(code = -1, message = "系统异常")
+    })
+    @PostMapping("/cloudWindowList")
+    public BaseResponse<PageInfo<EhbExhibitor>> cloudWindowList(@ApiParam(hidden = true) @RequestParam Map paramMap) {
+        if(StringUtils.isEmpty(paramMap.get("pageNum"))){
+            return setResultError(403,"","当前页码不能为空！");
+        }
+        QueryWrapper<EhbExhibitor> ehbExhibitorQueryWrapper = new QueryWrapper<>();
+        ehbExhibitorQueryWrapper.eq("isdel",CommonDict.CORRECT_STATE);
+        ehbExhibitorQueryWrapper.eq(!StringUtils.isEmpty(paramMap.get("enterprisename")),"enterprisename",paramMap.get("enterprisename"));
+        ehbExhibitorQueryWrapper.eq(!StringUtils.isEmpty(paramMap.get("boothno")),"boothno",paramMap.get("boothno"));
+
+        Integer pageParam[] = pageValidParam(paramMap);
+        PageHelper.startPage(pageParam[0], pageParam[1]);
+        List<EhbExhibitor> ehbExhibitors = ehbExhibitorService.list(ehbExhibitorQueryWrapper);
+        Collections.shuffle(ehbExhibitors);
+        return setResultSuccess(new PageInfo<>(ehbExhibitors));
+    }
 }
