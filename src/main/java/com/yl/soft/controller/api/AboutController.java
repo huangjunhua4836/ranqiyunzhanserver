80d9ab15c9cd56d897aec8119fe3270981ce952e
xingdi <xingdi@DESKTOP-EDC60K6>
2020-09-28 22:00:53 +0800

    11

---
+++ src/main/java/com/yl/soft/controller/api/AboutController.java
@@ -0,0 +1,49 @@
+package com.yl.soft.controller.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yl.soft.controller.base.BaseController;
+import com.yl.soft.dto.base.ResultItem;
+import com.yl.soft.po.EhbAbout;
+import com.yl.soft.po.EnbOnline;
+import com.yl.soft.service.EhbAboutService;
+import com.yl.soft.service.EnbOnlineService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = { "C端模块-关于/首页在线招展" })
+@RestController
+@RequestMapping("/api")
+public class AboutController extends BaseController {
+
+	@Autowired
+	public EhbAboutService ehbAboutService;
+	
+	@Autowired
+	public EnbOnlineService enbOnlineService;
+
+	@ApiOperation(value = "获取关于", notes = "获取关于")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "token", value = "登陆标识", required = true, paramType = "query"), })
+	@PostMapping("/getAbout")
+	public ResultItem<EhbAbout> getAbout(String token) {
+		List<EhbAbout> ehbAbouts = ehbAboutService.lambdaQuery().eq(EhbAbout::getType, 1).list();
+		return ok(ehbAbouts.size() > 0 ? ehbAbouts.get(0) : null);
+	}
+	
+	@ApiOperation(value = "在线招展", notes = "在线招展")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "token", value = "登陆标识", required = true, paramType = "query"), })
+	@PostMapping("/getOnline")
+	public ResultItem<EnbOnline> getOnline(String token) {
+		List<EnbOnline> ehbAbouts = enbOnlineService.lambdaQuery().eq(EnbOnline::getType, 1).list();
+		return ok(ehbAbouts.size() > 0 ? ehbAbouts.get(0) : null);
+	}
+	
+}
