56d12a8945b6a9fbc1ec475e5177ce14b37d3207
xingdi <xingdi@DESKTOP-EDC60K6>
2020-09-29 19:50:41 +0800

    update

--- src/main/java/com/yl/soft/controller/api/HallController.java
+++ src/main/java/com/yl/soft/controller/api/HallController.java
@@ -1,20 +1,20 @@
-package com.yl.soft.controller.api;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.yl.soft.controller.base.BaseController;
-import com.yl.soft.service.EhbExhibitorService;
-
-import io.swagger.annotations.Api;
-
-@Api(tags = {"C端模块-虚拟展厅"})
-@RestController
-@RequestMapping("/hall")
-public class HallController extends BaseController{
-
-	@Autowired
-	public EhbExhibitorService ehbExhibitorService;
-	
-}
+//package com.yl.soft.controller.api;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.yl.soft.controller.base.BaseController;
+//import com.yl.soft.service.EhbExhibitorService;
+//
+//import io.swagger.annotations.Api;
+//
+//@Api(tags = {"C端模块-虚拟展厅"})
+//@RestController
+//@RequestMapping("/hall")
+//public class HallController extends BaseController{
+//
+//	@Autowired
+//	public EhbExhibitorService ehbExhibitorService;
+//	
+//}
