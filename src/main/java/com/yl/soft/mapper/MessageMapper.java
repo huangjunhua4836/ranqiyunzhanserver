5239733e63cd670904a710cb8e02065ee2e83641
xingdi <xingdi@DESKTOP-EDC60K6.lan>
2020-10-05 20:08:00 +0800

    update

---
+++ src/main/java/com/yl/soft/mapper/MessageMapper.java
@@ -0,0 +1,16 @@
+package com.yl.soft.mapper;
+
+import com.yl.soft.po.Message;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 消息 Mapper 接口
+ * </p>
+ *
+ * @author ${author}
+ * @since 2020-10-05
+ */
+public interface MessageMapper extends BaseMapper<Message> {
+
+}
