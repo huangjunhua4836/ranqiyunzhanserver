9ea0d5baf78312d97e80bf7a7602b743c8dcc5fa
huangjunhua <740920822@qq.com>
2020-10-08 14:14:51 +0800

    文件上传管理

--- src/main/java/com/yl/soft/controller/plantform/ResourceUpLoadController.java
+++ src/main/java/com/yl/soft/controller/plantform/ResourceUpLoadController.java
@@ -1,17 +1,12 @@
 package com.yl.soft.controller.plantform;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.yl.soft.common.constants.BaseApiConstants;
 import com.yl.soft.common.unified.entity.BaseResponse;
 import com.yl.soft.common.util.StringUtils;
 import com.yl.soft.controller.base.BaseController;
-import com.yl.soft.dict.CommonDict;
 import com.yl.soft.po.EhbLabel;
 import com.yl.soft.service.EhbLabelService;
-import com.yl.soft.vo.TableVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -21,11 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * <p>
- * 标签表 前端控制器
+ * 资料上传 前端控制器
  * </p>
  *
  * @author ${author}
@@ -37,42 +31,8 @@ public class ResourceUpLoadController extends BaseController {
     @Autowired
     public EhbLabelService ehbLabelService;
 
-    @GetMapping("/list")
-    public String list() {
-        return "label/list";
-    }
-
     /**
-     * 查询标签列表
-     * @param page
-     * @param limit
-     * @param ehbLabel
-     * @param startTime
-     * @param endTime
-     * @return
-     */
-    @GetMapping("/initTable")
-    @ResponseBody
-    public TableVo initTable(String page, String limit, EhbLabel ehbLabel, String startTime, String endTime) {
-        QueryWrapper<EhbLabel> ehbLabelQueryWrapper = new QueryWrapper<>();
-        ehbLabelQueryWrapper.like(!StringUtils.isEmpty(ehbLabel.getName()),"name",ehbLabel.getName());
-        ehbLabelQueryWrapper.between(!StringUtils.isEmpty(startTime) && !StringUtils.isEmpty(endTime),"createtime",startTime,endTime);
-        ehbLabelQueryWrapper.eq("isdel", CommonDict.CORRECT_STATE);
-        ehbLabelQueryWrapper.orderByDesc("createtime");
-        PageHelper.startPage(Integer.valueOf(page),Integer.valueOf(limit));
-        List<EhbLabel> ehbLabels = ehbLabelService.list(ehbLabelQueryWrapper);
-        PageInfo pageInfo = new PageInfo<>(ehbLabels);
-
-        TableVo tableVo = new TableVo();
-        tableVo.setCode(0);
-        tableVo.setMsg("");
-        tableVo.setCount((int)pageInfo.getTotal());
-        tableVo.setData(pageInfo.getList());
-        return tableVo;
-    }
-
-    /**
-     * 跳转到单个标签添加或者修改页面
+     * 跳转到单个添加或者修改页面
      * @param id
      * @return
      */
@@ -86,11 +46,11 @@ public class ResourceUpLoadController extends BaseController {
         }
         modelMap.put("ehbLabel",ehbLabel);
 
-        return "label/input";
+        return "resourceupload/input";
     }
 
     /**
-     * 添加或者修改标签
+     * 添加或者修改
      * @param ehbLabel
      * @return
      */
@@ -113,7 +73,7 @@ public class ResourceUpLoadController extends BaseController {
     }
 
     /**
-     * 删除标签
+     * 删除
      * @return
      */
     @PostMapping("/delete")
