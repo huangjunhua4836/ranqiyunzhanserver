02e2eadf37b87af7be2269e9b3e8d61a6f198833
xingdi <xingdi@DESKTOP-EDC60K6.lan>
2020-10-14 10:33:41 +0800

    update

--- src/main/java/com/yl/soft/controller/api/PersonalCenterController.java
+++ src/main/java/com/yl/soft/controller/api/PersonalCenterController.java
@@ -32,6 +32,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Api(tags = { "C端模块-个人中心" })
@@ -101,7 +103,6 @@ public class PersonalCenterController extends BaseController {
 	public BaseResult<EhbAudienceDto> upMe(EhbAudienceInfoDto ehbAudienceInfoDto) {
 		SessionUser sessionUser = sessionState.getCurrentUser(ehbAudienceInfoDto.getToken());
 		if(null==sessionUser.getBopie()) {
-			
 			EhbAudience ehbAudience=ehbAudienceService.getById(sessionUser.getId());
 			if(StringUtils.isNoneBlank(ehbAudienceInfoDto.getEhbAudienceInfogrDto().getName())) {
 				ehbAudience.setName(ehbAudienceInfoDto.getEhbAudienceInfogrDto().getName());
@@ -128,10 +129,18 @@ public class PersonalCenterController extends BaseController {
 				ehbExhibitor.setPhone(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getPhone());
 			}
 			if(StringUtils.isNoneBlank(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getMailbox())) {
-				ehbExhibitor.setMailbox(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getMailbox());
+		        if(isMobile(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getMailbox())){
+		        	ehbExhibitor.setMailbox(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getMailbox());
+		        }else{
+		        	return error(-100,"请输入一个正确的邮件地址");
+		        }
 			}
 			if(StringUtils.isNoneBlank(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getTelphone())) {
-				ehbExhibitor.setTelphone(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getTelphone());
+				if(isPhone(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getTelphone())) {
+					ehbExhibitor.setTelphone(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getTelphone());
+				}else {
+					return error(-100,"请输入一个正确的电话号码");
+				}
 			}
 			if(StringUtils.isNoneBlank(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getExname())) {
 				ehbExhibitor.setName(ehbAudienceInfoDto.getEhbAudienceInfoyeDto().getExname());
@@ -142,6 +151,58 @@ public class PersonalCenterController extends BaseController {
 		}
 		return ok();
 	}
+	
+	public static void main(String[] args) {
+	    String phone = "13900442200";
+	    String phone2 = "021-88889999";
+	    String phone3 = "88889999";
+	    String phone4 = "1111111111";
+	    //测试1
+	    if(isPhone(phone)){
+	      System.out.println("1这是符合的");
+	    }
+	    //测试2
+	    if(isPhone(phone2)){
+	      System.out.println("2这是符合的");
+	    }
+	    //测试3
+	    if(isPhone(phone3)){
+	      System.out.println("3这是符合的");
+	    }
+	    //测试4
+	    if(isPhone(phone4)){
+	      System.out.println("4这是符合的");
+	    }else{
+	      System.out.println("不符合");
+	    }
+	  }
+
+	  public static boolean isMobile(final String str) {
+	      Pattern p = null;
+	      Matcher m = null;
+	      boolean b = false;
+	      p = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$"); // 验证手机号
+	      m = p.matcher(str);
+	      b = m.matches();
+	      return b;
+	  }
+
+	
+	public static boolean isPhone(final String str) {
+	      Pattern p1 = null, p2 = null;
+	      Matcher m = null;
+	      boolean b = false;
+	      p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
+	      p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
+	      if (str.length() > 9) {
+	         m = p1.matcher(str);
+	         b = m.matches();
+	      } else {
+	          m = p2.matcher(str);
+	         b = m.matches();
+	      }
+	      return b;
+	  }
 
 	@ApiOperation(value = "我的企业", notes = "我的企业")
 	@ApiImplicitParams({ @ApiImplicitParam(name = "token", value = "登陆标识", required = true, paramType = "query"), })
