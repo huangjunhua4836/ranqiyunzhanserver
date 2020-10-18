43bde81fe5712d7c43f527971a83bd2bbeff6066
huangjunhua <740920822@qq.com>
2020-10-18 20:04:32 +0800

    add

--- src/main/java/com/yl/soft/controller/api/RegisterController.java
+++ src/main/java/com/yl/soft/controller/api/RegisterController.java
@@ -125,10 +125,10 @@ public class RegisterController extends BaseController {
             }
             ehbAudience.setLabelid(JSONArray.toJSONString(labs));
         }else{//没有选择标签选择前5个
-            List<Integer> labs = ehbLabelService.lambdaQuery().last("limit 5").list().stream().map(i->{
-               return i.getId();
-            }).collect(Collectors.toList());
-            ehbAudience.setLabelid(JSONArray.toJSONString(labs));
+//            List<Integer> labs = ehbLabelService.lambdaQuery().last("limit 5").list().stream().map(i->{
+//               return i.getId();
+//            }).collect(Collectors.toList());
+//            ehbAudience.setLabelid(JSONArray.toJSONString(labs));
         }
 
         if(ehbAudienceService.updateById(ehbAudience)){
@@ -225,10 +225,10 @@ public class RegisterController extends BaseController {
             }
             ehbExhibitor.setLabelid(JSONArray.toJSONString(labs));
         }else{//没有选择标签选择前5个
-            List<Integer> labs = ehbLabelService.lambdaQuery().last("limit 5").list().stream().map(i->{
-                return i.getId();
-            }).collect(Collectors.toList());
-            ehbExhibitor.setLabelid(JSONArray.toJSONString(labs));
+//            List<Integer> labs = ehbLabelService.lambdaQuery().last("limit 5").list().stream().map(i->{
+//                return i.getId();
+//            }).collect(Collectors.toList());
+//            ehbExhibitor.setLabelid(JSONArray.toJSONString(labs));
         }
         if(ehbExhibitorService.saveExhibitor(ehbAudience,ehbExhibitor)){
             return ok2();
