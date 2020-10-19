52f743a416adab66bd8ceddf1610eb425b0d5d36
huangjunhua <740920822@qq.com>
2020-10-19 22:45:45 +0800

    add

--- src/main/resources/static/js/role/role.js
+++ src/main/resources/static/js/role/role.js
@@ -47,7 +47,7 @@ layui.use('core', function(){
     table.on('toolbar(tableFilter)', function(obj){
         switch(obj.event){
             case 'add':
-                core.openIframeDialog('添加','/crmRole/input?type=add',['500px', '200px'],false,initTable);
+                core.openIframeDialog('添加','/crmRole/input?type=add',['100%', '90%'],false,initTable);
                 break;
             //自定义头工具栏右侧图标 - 提示
             case 'LAYTABLE_TIPS':
@@ -70,9 +70,9 @@ layui.use('core', function(){
                 }
             });
         } else if(obj.event === 'edit'){
-            core.openIframeDialog('修改','/crmRole/input?type=update&id='+data.id,['500px', '200px'],false,initTable);
+            core.openIframeDialog('修改','/crmRole/input?type=update&id='+data.id,['100%', '90%'],false,initTable);
         } else if(obj.event === 'detail'){
-            core.openDialog('详情',$('#detail').html(),['500px','480px']);
+            core.openDialog('详情',$('#detail').html(),['100%', '90%']);
             $('.layui-layer-content').find('input').eq(0).val(data.id);
             $('.layui-layer-content').find('input').eq(1).val(data.name);
             $('.layui-layer-content').find('input').eq(2).val(data.createUser);
